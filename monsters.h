4bbd9569dcc1fec8d5e7c24ce393dd015a616e57
SMARCOCCIA <seb@gmail.fr>
2026-03-19 18:23:12 +0100

    Ajout de fonctions et de commentaires

--- monsters.h
+++ monsters.h
@@ -3,7 +3,6 @@
 
 #define MST_MAX_SLOTS 64 // 64 monstres max à l'écran.
 
-#define MST_SPEED_NOT 0
 #define MST_SPEED_INIT 0x080 // Vitesse testé avec le jeu d'origine (lolo 1) donc ok 0x80
 #define MST_SPEED_MAX 0x200  // 0x100
 
@@ -35,13 +34,13 @@ extern SMstsLvls gMstsLvls[];
 // Noms des monstres.
 enum EMstName{
     // En mouvement :
-    e_Mst_Alma, 
-    e_Mst_Rocky, 
-    e_Mst_Medusa,
+    e_Mst_Alma,
+    e_Mst_Leeper, // Si collision devient static.
+    e_Mst_Rocky,
+    e_Mst_Skull, // Static tant que le joueur n'a pas récupéré tout les coffres.
     e_Mst_DonMedusa,    
     // Static :
-    e_Mst_Leeper, // Si collision
-    e_Mst_Skull, // Après récupéré le diamant du coffre
+    e_Mst_Medusa,
     e_Mst_Snakey,
     e_Mst_Gol,
     e_Mst_Whale,
@@ -55,8 +54,8 @@ enum EMstProperties{
 
 enum EMstState{
     e_MstAnim_Idle,
+    e_MstAnim_Walk, // ou Jumping
     e_MstAnim_Sleeping,
-    e_MstAnim_Walk,
     e_MstAnim_Win,
     e_MstAnim_Death,
     e_MstAnim_Max,
@@ -83,19 +82,30 @@ bool bCollidePlyr; // Collision avec le player. Pour éviter le recallage si col
 
 // Note : gMst peut-ê remplacé par gpMstSlots
 extern SMonster gMst[MST_MAX_SLOTS];
-
+// Recalage du plyer après collision avec le Monster
+void Monster_CollideAdjusting(u32 nPX, u32 nMX, SMonster *pMst);
+// Test si le monstre peut se déplacer dans la direction demandée. Utilisé pour les murs, décore (arbres, pierre, ...), items (coeurs, blocs, eggs, ...)
+// Out : true = déplacement ok, false = ne doit pas gouger.
+bool Monster_CheckDepl(SMonster *pMst, u32 nDir, bool bIsCollide);
 // Recalage Mst
 void Game_MstRecalage(SMonster* pMst);
 // Test si le monstre à une anim. Retourne dans tous les cas un n° de sprites.
 // Utilisation de Animspr_GetImgLast dans la fonction.
 u32 Monsters_IfHasAnAnim(u32 nIdx);
-// Collision Monstre avec le Joueur.
-void Monster_CollidePlyr(void);
 // Si le perso est tué => on fait une init de fin, les monstres sont mit en idle.
 void Monsters_InitOfEnd(void);
-void Monsters_CollideMonster(u32 nIdxMst);
-// Collision avec le décore y compris les items (coffre, coeurs, block déplaçable ou oeuf (tire du plyr sur un mst)).
-bool Monsters_CollideDecor(u32 nIdx, u32 nLvl);
+// Collision Monstre avec le Joueur (contient Monsters_CollideDecor).
+void Monster_CollidePlyr(void);
+// Collision avec les items lorsque M et P même colonne.
+bool Monster_CollidePlyrItem(SMonster *pMst);
+// Collision avec le décore lorsque M et P son sur la même colonne et que M à côté de S à côté de P (M S P).
+// Out : true = bloc est un solide, false = n'est pas un solide.
+bool Monster_CollideDecorPlyr(SMonster *pMst, u32 nNbBlk);
+// Collision avec le décore lorsque monster et player son sur la même colonne et que M à côté de P à côté de S (M P S).
+// nNbBlk est le nombre de blocs qui séparent le M et le S inclue.
+bool Monster_CollidePlyrDecor(SMonster *pMst, u32 nNbBlk);
+// Collision avec le décore y compris les items (coffre, coeurs, block déplaçable ou oeuf (tire du plyr sur un mst)) et monstre statique.
+bool Monsters_CollideDecor(SMonster *pMst, u32 nLvl);
 // Collision mur :
 void Monsters_CollideWalls(u32 nIdxMst);
 // RAZ des slots.
@@ -113,5 +123,7 @@ s32 Monsters_InitMonsters_Aux(u32 nNoSlotMst, u32 nNoMst, u64 *pAnim, u64 nAnimN
 // Gestion des mnostres (collision, etc). A faire
 void Monsters_Manage(void);
 void Monster_Recalage(struct SMonster *pMst, u32 nLastPosX, u32 nLastPosY);
+// Collision Snake avec joueur.
+bool Monster_CollideSnake(SMonster *pMst);
 
 
